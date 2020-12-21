7841d0378291ca93637234d7b2e52dfd94769189
chardbres <richard.m.bresnahan@gmail.com>
2020-12-21 17:14:02 -0500

    Implemented Material-UI theming in Button component. Successfully integrated Firebase Auth API and confirmed successful signup of a new user. Incorporated react-router-dom to add successful routing on signup.

---
+++ yeswedo-react-refactor/src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { HomePage } from './HomePage/HomePage'
+export { LandingPage } from './LandingPage/LandingPage'
+export { SignInPage } from './SignInPage/SignInPage'
+export { SignUpPage } from './SignUpPage/SignUpPage'
