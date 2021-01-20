909bc5a6a3b4e50b33af7be35b3a79fa115c8f57
chardbres <richard.m.bresnahan@gmail.com>
2021-01-19 21:25:57 -0500

    Successfully pulled bills data from Firestore and wrote to store.

--- yeswedo-react-refactor/src/api/Firebase/firebase.js
+++ yeswedo-react-refactor/src/api/Firebase/firebase.js
@@ -1,6 +1,7 @@
 import app from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import 'firebase/firestore'
 
 const config = {
     apiKey: "AIzaSyC0JCDZ0qYnGimDZ60_sNXUS7X4jMoyAoI",
@@ -17,7 +18,8 @@ class Firebase {
     app.initializeApp(config)
 
     this.auth = app.auth()
-    this.db = app.database()
+    this.firestore = app.firestore()
+    this.data = []
   }
 
   // Auth API
@@ -35,7 +37,18 @@ class Firebase {
     this.auth.currentUser.updatePassword(password)
 
   // Database API
-  doGetDashboardData = () => this.db.ref()
+  doGetDashboardData = (myUID) => { 
+    this.firestore.collection('Bill Fanout').where('Client', '==', `${myUID}`).onSnapshot(snapshot => {
+      snapshot.forEach(item => {
+        this.data.push(item.data())
+      })
+    })
+  }
+
+  returnData = () => {
+    return this.data
+  }
+
 }
    
 export default Firebase
--- yeswedo-react-refactor/src/pages/Dashboard/Dashboard.tsx
+++ yeswedo-react-refactor/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import { compose } from 'recompose'
 import { withFirebase } from '../../api/Firebase'
 import { Header, Menu } from '../../components/organisms'
@@ -28,30 +28,14 @@ const Dashboard = props => {
     const { addBillsData, addJobsData } = props
 
     useEffect(() => {
-        const billArray : any[] = []
-        const jobArray : any[] = []
-        // Get bills by user and add to store
-        props.firebase
-            .doGetDashboardData().child('Bill Fanout').child(activeUser.uid).orderByChild('TimeStamp').startAt(startTime[0]).on('child_added', snap => {
-                let bill = snap.val()
-                if (billArray.length === 0) {
-                    billArray.push(bill)
-                }
-                billArray.forEach((element, index) => {
-                    element.Job === bill.Job && element.TimeStamp <= bill.TimeStamp ? billArray[index] = bill : billArray.push(bill)
-                })
-            })
-        addBillsData(billArray)
+        async function getBillsData() {
+            props.firebase.doGetDashboardData(activeUser.uid)
+        }
 
-        props.firebase
-            .doGetDashboardData().child('Jobs Board Fanout').child(activeUser.uid).once('value', snap => {
-                let job = snap.val()
-                jobArray.push(job)
-            })
-        addJobsData(jobArray)
+        getBillsData()
+            .then(() => addBillsData(props.firebase.returnData()))
         
     },[])
-
 
     return (
         <div>
@@ -78,7 +62,9 @@ const mapDispatchToProps = dispatch => {
 }
 
 const mapStateToProps = state => {
-    return { activeUser: state.activeUser }
+    return { 
+        activeUser: state.activeUser
+    }
 }
 
 export default compose(
@@ -95,15 +81,3 @@ const detailCSS = css`
     display: flex;
     flex-direction: row;
 `
-
-// db.collection('Bill Fanout')
-//           .where('Client', '==', `${myUID}`)
-//           .orderBy('TimeStamp')
-//           .onSnapshot(async function (querySnapshot) {
-//             var info = [];
-//             querySnapshot.forEach(async function(doc) {
-//               let element = doc.data();
-//               console.log(element["TimeStamp"]);
-//               await parseBills(element, user.uid);
-//             });
-//           });
